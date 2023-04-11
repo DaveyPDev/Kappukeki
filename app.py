14e0f3e4aebda0dc644b53729f96e8306e9213f2
DaveyPDev <74942934+DaveyPDev@users.noreply.github.com>
2023-04-11 16:27:20 -0400

    Add files via upload
    start

---
+++ app.py
@@ -0,0 +1,52 @@
+"""Flask app for Cupcakes"""
+
+from flask import Flask, render_template, jsonify, request
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'not-so-secret'
+
+connect_db(app)
+
+@app.route('/')
+def root():
+    """ Homepage """
+
+    return render_template('index.html')
+
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    """ All Cupcake info """
+
+    cupcakes = [cupcake.to_dict() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_cupcake():
+    """ New Cupcake """
+
+    data = request.json
+    
+    cupcake = Cupcake(
+        flavor=data['flavor'],
+        rating=data['rating'],
+        size=data['image'] or None
+    )
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return (jsonify(cupcake=cupcake.to_dict()), 201)
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def single_cupcake(cupcake_id):
+    """ Single Cupcake Info by ID """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    return jsonify(cupcake=cupcake.to_dict())
+
---
+++ models.py
@@ -0,0 +1,33 @@
+"""Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1612203985729-70726954388c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80'
+
+class Cupcake(db.Model):
+    """ Cupcake Info """
+
+    __tablename__ = 'cupcakes'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+    def dict_cupcake(self):
+        """ Serialize info """
+
+        return {
+            'id': self.id,
+            'flavor': self.flavor,
+            'rating': self.rating,
+            'image': self.image
+        }
+    
+def connect_db(app):
+    """ Connect to Database """
+
+    db.app = app
+    db.init_app(app)
